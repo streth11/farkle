1c2d091f15ae5bb599658e9ec7c3964de115ec41
streth11 <tom.streuli@gmail.com>
2024-01-13 12:44:28 +0000

    impliment basic strategy

--- main.py
+++ main.py
@@ -1,6 +1,77 @@
 from hand import Hand
 from scoring import diceScore, Score
 
+
+class Strategy():
+    hand = None
+    roll_score = 0
+
+    @classmethod
+    def update_score(cls,hand: Hand,roll_score: Score):
+        cls.hand = hand
+        cls.roll_score = roll_score
+        return cls
+
+    def onNoScore(self):
+        self.hand.fix(range(0,6))
+        return 0
+    
+    def onBigScore(self):
+        self.hand.fix(self.roll_score.big.idxs)           
+        return self.roll_score.big.value
+    
+    def onTripletScore(self):
+        self.hand.fix(self.roll_score.triplet.idxs)           
+        return self.roll_score.triplet.value
+    
+    def on5Score(self):
+        return 5
+    
+    def on1Score(self):
+        return 1
+
+
+class Turn():
+    def __init__(self, init_hand: Hand=Hand(), strategy=Strategy()) -> None:
+        self.hand = init_hand
+        self.__strategy = strategy
+        self.roll_score = Score()
+        self.score = []
+    
+    @property
+    def totalScore(self):
+        return sum(self.score)
+    
+    def play(self):
+        # self.hand.roll()
+        self.roll_score = Score.dice(self.hand)
+        self.executeStrategy()
+        return self
+
+    def executeStrategy(self):
+        '''
+        If roll_score == 0
+        '''
+        self.__strategy.update_score(self.hand, self.roll_score)
+
+        if self.roll_score.total == 0:
+            score = self.__strategy.onNoScore()
+        if self.roll_score.big.value:
+            score = self.__strategy.onBigScore()
+
+        if self.hand.n_fixed == 6:
+            self.score.append(score)
+            if score == 0:
+                return self
+            # hot dice
+            # self.play()
+
+        if self.roll_score.triplet.value:
+            score = self.__strategy.onTripletScore()
+
+        return self
+
+
 if __name__ == "__main__":
     h = Hand()
     print(h)
@@ -14,8 +85,9 @@ if __name__ == "__main__":
     print(h.fixes)
     print(h.n_fixed)
 
-
     s = Score.dice(Hand([3, 3, 3, 3, 3, 1]))
     print(s)
 
-
+    t = Turn(Hand([3, 3, 3, 3, 3, 1]))
+    t.play()
+    x=1
--- scoring.py
+++ scoring.py
@@ -2,8 +2,14 @@ import numpy as np
 
 from hand import Hand
 
+class ScoreElement():
+    def __init__(self, value, idxs = np.zeros(6)):
+        self.value = value
+        self.idxs = idxs
+
 class Score():
-    def __init__(self, big=0, triplet=0, ones=0, fives=0):
+    def __init__(self, big=ScoreElement(0), triplet=ScoreElement(0),
+                 ones=ScoreElement(0), fives=ScoreElement(0)):
         self.big = big
         self.triplet = triplet
         self.ones = ones
@@ -11,7 +17,7 @@ class Score():
     
     @property
     def score_list(self):
-        return [self.big, self.triplet, self.ones, self.fives]
+        return [self.big.value, self.triplet.value, self.ones.value, self.fives.value]
     
     @property
     def total(self):
@@ -34,17 +40,18 @@ def diceScore(dice: Hand):
     score.big = bigDiceScore(dice)
     score.triplet = tripletScore(dice)
                  
-    DICE_SCORES = [100,0,0,0,50,0]
-    score.ones = singleScore(dice, 1, DICE_SCORES)
-    score.fives = singleScore(dice, 5, DICE_SCORES)
+    score.ones = singleScore(dice, 1)
+    score.fives = singleScore(dice, 5)
 
     return score
 
-
-def singleScore(dice: Hand, dice_number, dice_scores = [100,0,0,0,50,0]):
+def singleScore(dice: Hand, dice_number):
     # single scores
-    score = dice.count_list[dice_number-1] * dice_scores[dice_number-1]
-    return score
+    DICE_SCORES = [100,0,0,0,50,0]
+    score_value = dice.count_list[dice_number-1] * DICE_SCORES[dice_number-1]
+    scoring_idx = np.where(dice.dice_array == dice_number)[0]
+    return ScoreElement(score_value,scoring_idx)
+
 
 def tripletScore(dice: Hand):
 
@@ -53,9 +60,12 @@ def tripletScore(dice: Hand):
         if dice.count_of_counts["3 of a kinds"] == 1:
             # 3 of a kind
             dice_num_3kind_idx = (np.where(dice.count_list == 3))[0][0]+1
-            dice.fix(np.where(dice.dice_array == dice_num_3kind_idx)[0])
-            return DICE3_SCORES[dice_num_3kind_idx]
-    return 0
+            score_value = DICE3_SCORES[dice_num_3kind_idx]
+            scoring_idx = np.where(dice.dice_array == dice_num_3kind_idx)[0]
+            # dice.fix(scoring_idx)
+            return ScoreElement(score_value,scoring_idx)
+    return ScoreElement(0)
+
 
 def bigDiceScore(dice: Hand):
 
@@ -63,37 +73,39 @@ def bigDiceScore(dice: Hand):
         # scores that use all 6 dice
         if all(dice.count_list == 1):
             # straight 1-6
-            dice.fix(range(0,6))
-            return 1500
+            # dice.fix(range(0,6))
+            return ScoreElement(1500,range(0,6))
         if dice.count_of_counts["3 of a kinds"] == 2:
             # 2 triplets
-            dice.fix(range(0,6))
-            return 2500
+            # dice.fix(range(0,6))
+            return ScoreElement(2500,range(0,6))
         if dice.count_of_counts["2 of a kinds"] == 3:
             # 3 pairs
-            dice.fix(range(0,6))
-            return 1501
+            # dice.fix(range(0,6))
+            return ScoreElement(1501,range(0,6))
         if dice.count_of_counts["4 of a kinds"] == 1 and dice.count_of_counts["2 of a kinds"] == 1:
             # 4 of a kind and a pair (full house)
-            dice.fix(range(0,6))
-            return 1502
+            # dice.fix(range(0,6))
+            return ScoreElement(1502,range(0,6))
         if dice.count_of_counts["6 of a kinds"] == 1:
             # 6 of a kind
-            dice.fix(range(0,6))
-            return 3000
+            # dice.fix(range(0,6))
+            return ScoreElement(3000,range(0,6))
 
     if dice.n_fixed <= 1:
         if dice.count_of_counts["5 of a kinds"] == 1:
             # 5 of a kind
             dice_num_5kind = (np.where(dice.count_list == 5))[0][0]+1
-            dice.fix(np.where(dice.dice_array == dice_num_5kind)[0])
-            return 2000
+            scoring_idx = np.where(dice.dice_array == dice_num_5kind)[0]
+            # dice.fix(scoring_idx)
+            return ScoreElement(2000,scoring_idx)
 
     if dice.n_fixed <= 2:
         if dice.count_of_counts["4 of a kinds"] == 1:
             # 4 of a kind
             dice_num_4kind = (np.where(dice.count_list == 4))[0][0]+1
-            dice.fix(np.where(dice.dice_array == dice_num_4kind)[0])
-            return 1500
+            scoring_idx = np.where(dice.dice_array == dice_num_4kind)[0]
+            # dice.fix(scoring_idx)
+            return ScoreElement(1500,scoring_idx)
         
-    return 0
+    return ScoreElement(0)
